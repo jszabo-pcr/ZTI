04f7ee8df4234d575adf80948498b26f9bcd3dc5
jszabo-pcr <szabo@pcrtechnology.com>
2025-05-30 09:35:26 -0400

    Update AutopilotEnroll&OSDCloudZTI.ps1

--- AutopilotEnroll&OSDCloudZTI.ps1
+++ AutopilotEnroll&OSDCloudZTI.ps1
@@ -231,3 +231,30 @@ Process {
         Write-Warning -Message "Failed to upload hardware hash. Request failed with HTTP Status $($_.Exception.Response.StatusCode) and description: $($_.Exception.Response.StatusDescription)"
     }  
 }
+
+
+# Settings
+$OSName = 'Windows 11 24H2 x64'
+$OSEdition = 'Enterprise'
+$OSActivation = 'Retail'
+$OSLanguage = 'en-US'
+#$GroupTag = 'AutopilotUserDeployment'
+
+#Set Global OSDCloud Vars
+$Global:MyOSDCloud = [ordered]@{
+    BrandColor = "#0096FF"
+    Restart = [bool]$true
+    RecoveryPartition = [bool]$True
+    OEMActivation = [bool]$True
+    WindowsUpdate = [bool]$True
+    WindowsUpdateDrivers = [bool]$True
+    WindowsDefenderUpdate = [bool]$True
+    SetTimeZone = [bool]$True
+    ClearDiskConfirm = [bool]$False
+    ShutdownSetupComplete = [bool]$false
+    SyncMSUpCatDriverUSB = [bool]$True
+    CheckSHA1 = [bool]$True
+}
+
+Write-Host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage"
+Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
