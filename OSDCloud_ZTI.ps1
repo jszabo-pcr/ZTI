3eed55ae742d3e39d406fba50f7f5372de876289
jszabo-pcr <szabo@pcrtechnology.com>
2025-05-30 13:14:02 -0400

    Update OSDCloud_ZTI.ps1

--- OSDCloud_ZTI.ps1
+++ OSDCloud_ZTI.ps1
@@ -21,5 +21,28 @@ $Global:MyOSDCloud = [ordered]@{
     CheckSHA1 = [bool]$True
 }
 
-Write-Host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage"
-Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
+
+Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
+Start-Sleep -Seconds 5
+
+#Change Display Resolution for Virtual Machine
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host -ForegroundColor Green "Setting Display Resolution to 1600x"
+    Set-DisRes 1600
+}
+
+#Make sure I have the latest OSD Content
+Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
+Install-Module OSD -Force
+
+Write-Host -ForegroundColor Green "Importing OSD PowerShell Module"
+Import-Module OSD -Force
+
+#Start OSDCloud ZTI the RIGHT way
+Write-Host -ForegroundColor Green "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage -ZTI"
+Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage -ZTI
+
+#Restart from WinPE
+Write-Host -ForegroundColor Green "Restarting in 5 seconds!"
+Start-Sleep -Seconds 5
+wpeutil reboot
